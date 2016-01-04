c790aff6d1f1b7d592ca870b58fa7fb9a84d1d04
Pranav Gandhi <pranavrulz@gmail.com>
2016-01-04 01:01:17 -0500

    Build / Run _something_

---
+++ README.md
@@ -0,0 +1,10 @@
+Its Been Ages
+=============
+
+Top Secret stuff.
+
+# Building / Running the app
+```
+mvn clean package
+java -jar target/ItsBeenAges-1.0-SNAPSHOT.jar
+```
---
+++ src/main/java/com/itsbeenages/ItsBeenAges.java
@@ -0,0 +1,11 @@
+package com.itsbeenages;
+
+/**
+ * Created by pgandhi on 1/4/16.
+ */
+public class ItsBeenAges {
+
+  public static void main(String[] args) {
+    System.out.println("HELLO WORLD.");
+  }
+}
